423c22e8a5c2a37835ec0c60a4c06ecb04ad65ab
ЫФВЫФВЫВ <salvadorderiony@gmail.com>
2025-10-05 06:16:57 +0300

    NewClass1 created constructor. NewClassBodyUtils was created. But empty.

--- Test_Project_Commits/src/main/java/NewClass1.java
+++ Test_Project_Commits/src/main/java/NewClass1.java
@@ -3,4 +3,19 @@ public class NewClass1 {
     String name;
     int age;
     int legAmount;
+
+    public NewClass1(String name, int age, int legAmount) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getLegAmount() {
+        return legAmount;
+    }
 }
