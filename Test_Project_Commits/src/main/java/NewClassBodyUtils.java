292af1f22c1be95a9d7011aa90814c8d82a557ec
ЫФВЫФВЫВ <salvadorderiony@gmail.com>
2025-10-05 16:50:57 +0300

    Revert "NewClassBodyUtils was created. Created first "body"."
    This reverts commit fe0f943c

--- Test_Project_Commits/src/main/java/NewClassBodyUtils.java
+++ Test_Project_Commits/src/main/java/NewClassBodyUtils.java
@@ -1,19 +1,4 @@
 public class NewClassBodyUtils {
 
     NewClass1 body1 = new NewClass1("Name1", 11, 2);
-    NewClass1 body2 = new NewClass1("Name2", 111, 2);
-    NewClass1 body3 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body4 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body5 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body6 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body7 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body8 = new NewClass1("Name3", 1111, 2);
-
-    NewClass1[] bodies = {body1, body2, body3, body4, body5, body6, body7, body8};
-    NewClass1[] bodies2 = {body1, body2, body3, body4, body5, body6, body7, body8};
-    NewClass1[] bodies3 = {body1, body2, body3, body4, body5, body6, body7, body8};
-    NewClass1[] bodies4 = {body1, body2, body3, body4, body5, body6, body7, body8};
-    NewClass1[] bodies5 = {body1, body2, body3, body4, body5, body6, body7, body8};
-    NewClass1[] bodies6 = {body1, body2, body3, body4, body5, body6, body7, body8};
-    NewClass1[] bodies7 = {body1, body2, body3, body4, body5, body6, body7, body8};
 }
