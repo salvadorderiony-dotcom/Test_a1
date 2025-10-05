3650cb50541497171398c57506eaa2b784f170a9
ЫФВЫФВЫВ <salvadorderiony@gmail.com>
2025-10-05 07:55:01 +0300

    NewClassBodyUtils fixed names of all bodies. Ready to "push". First PUSH. NOW.

--- Test_Project_Commits/src/main/java/NewClassBodyUtils.java
+++ Test_Project_Commits/src/main/java/NewClassBodyUtils.java
@@ -3,11 +3,11 @@ public class NewClassBodyUtils {
     NewClass1 body1 = new NewClass1("Name1", 11, 2);
     NewClass1 body2 = new NewClass1("Name2", 111, 2);
     NewClass1 body3 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body3 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body3 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body3 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body3 = new NewClass1("Name3", 1111, 2);
-    NewClass1 body3 = new NewClass1("Name3", 1111, 2);
+    NewClass1 body4 = new NewClass1("Name3", 1111, 2);
+    NewClass1 body5 = new NewClass1("Name3", 1111, 2);
+    NewClass1 body6 = new NewClass1("Name3", 1111, 2);
+    NewClass1 body7 = new NewClass1("Name3", 1111, 2);
+    NewClass1 body8 = new NewClass1("Name3", 1111, 2);
 
-
+    NewClass1[] bodies = {body1, body2, body3, body4, body5, body6, body7, body8};
 }
